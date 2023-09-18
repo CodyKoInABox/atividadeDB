04915edc20f9f6098e574be6326051f8029a71ef
CodyKoInABox <codykoinabox@gmail.com>
2023-09-18 13:52:02 -0300

    Exercicio completo

--- atividade.session.sql
+++ atividade.session.sql
@@ -57,3 +57,94 @@ SELECT * FROM Empregados;
 UPDATE Empregados
 SET nome = "Pedro"
 WHERE nome = "Jose"
+
+-- @block
+ALTER TABLE Empregados
+ADD email VARCHAR(20)
+
+-- @block
+ALTER TABLE Empregados
+MODIFY email VARCHAR(40)
+
+-- @block
+UPDATE Empregados
+SET email = "victor2001@gmail.com"
+WHERE nome = "Victor"
+
+-- @block
+UPDATE Empregados
+SET email = "pedrinho02@gmail.com"
+WHERE nome = "Pedro"
+
+-- @block
+UPDATE Empregados
+SET email = "marcospolos@gmail.com"
+WHERE nome = "Marcos"
+
+-- @block
+UPDATE Empregados
+SET email = "ricaricardo@gmail.com"
+WHERE nome = "Ricardo"
+
+-- @block
+UPDATE Empregados
+SET email = "lucio98@gmail.com"
+WHERE nome = "Lucio"
+
+-- @block
+ALTER TABLE Departamentos
+ADD cidade VARCHAR(30)
+
+-- @block
+UPDATE Departamentos
+SET cidade = "Joinville"
+
+-- @block
+SELECT * FROM Departamentos
+
+-- @block
+CREATE TABLE Cidades(
+    codigo INT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(50) NOT NULL,
+    uf CHAR(2) NOT NULL,
+    PRIMARY KEY (codigo)
+)
+
+-- @block
+INSERT INTO Cidades(nome, uf) VALUES("Joinville", "SC");
+INSERT INTO Cidades(nome, uf) VALUES("Blumenau", "SC");
+INSERT INTO Cidades(nome, uf) VALUES("Florianopolis", "SC");
+
+-- @block
+ALTER TABLE Departamentos
+DROP COLUMN cidade
+
+-- @block
+ALTER TABLE Departamentos
+ADD cod_cidade INT
+
+-- @block
+ALTER TABLE Departamentos
+ADD FOREIGN KEY (cod_cidade) REFERENCES Cidades(codigo)
+
+-- @block
+SELECT * FROM Cidades
+
+-- @block
+UPDATE Departamentos
+SET cod_cidade = 1
+
+-- @block
+CREATE TABLE Estado(
+    codigo INT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(50),
+    sigla CHAR(2) NOT NULL UNIQUE,
+    PRIMARY KEY (codigo)
+)
+
+-- @block
+INSERT INTO Estado(nome, sigla) VALUES("Santa Catarina", "SC");
+INSERT INTO Estado(nome, sigla) VALUES("Sao Paulo", "SP");
+
+-- @block
+DROP TABLE Estado
